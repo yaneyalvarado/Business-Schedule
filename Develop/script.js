68e1526036d17d2c4f570bb8e6500005b9c09780
yaneyalvarado <alvyaney@gmail.com>
2023-03-22 09:31:52 -0600

    added var in js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var todayDate = moment(). format('dddd, MMM Do YYY');
+$("#currentDay").html(todayDate);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
