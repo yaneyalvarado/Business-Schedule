e5439093b1c65422a96afa702750da37267e12b7
yaneyalvarado <alvyaney@gmail.com>
2023-03-23 17:35:42 -0600

    updated line 38 and 39 in js

--- Develop/script.js
+++ Develop/script.js
@@ -34,6 +34,10 @@ $(".time-block").each(function() {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+  if (blockTime < timeNow) {
+    
+  }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
