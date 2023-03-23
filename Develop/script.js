9b23fee431124f5eec66722d1ce25052b081a9a4
yaneyalvarado <alvyaney@gmail.com>
2023-03-23 17:29:57 -0600

    updated js

--- Develop/script.js
+++ Develop/script.js
@@ -5,21 +5,24 @@
 var todayDate = moment(). format('dddd, MMM Do YYY');
 $("#currentDay").html(todayDate);
 
+// TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+  
 $(document).ready(function () {
   $(".saveBtn").on("click" , function() {
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
   })
 
+
 $(".time-block").each(function() {
   var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 })
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
