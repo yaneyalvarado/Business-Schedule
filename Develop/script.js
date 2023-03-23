859482cbc69bfa77a4993afecfa839e4b866889f
yaneyalvarado <alvyaney@gmail.com>
2023-03-23 17:41:46 -0600

    updated line 44 and 45 in js

--- Develop/script.js
+++ Develop/script.js
@@ -40,6 +40,10 @@ $(".time-block").each(function() {
     $(this).removeClass("present");
     $(this).removeClass("past");
   }
+
+  else if (block === timeNow) {
+    $(this).removeClass("past");
+  }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
