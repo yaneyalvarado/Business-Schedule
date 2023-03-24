c2ce98be6b12ae6a27b99a49fcd8d61f61ae7255
yaneyalvarado <alvyaney@gmail.com>
2023-03-23 22:01:00 -0600

    updated js code

--- Develop/script.js
+++ Develop/script.js
@@ -19,18 +19,35 @@ $("#currentDay").html(todayDate);
 $(document).ready(function () {
   $(".saveBtn").on("click" , function() {
     var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+    var time = $(this).parent().attr("id"); 
+    localStorage.setItem(time, text);
   })
 
   function timeTracker() {
     var timeNow = dayjs().hour();
-  }
-
+    // var timeNow = 11
+ 
 
 $(".time-block").each(function() {
-  var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-})
+  var blockTime = parseInt($(this).attr("id").split("-")[1]); 
+  console.log(blockTime)
+  if (blockTime < timeNow) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+  } else if (blockTime === timeNow) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");  
   
+  } else {
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  }
+}) }
+timeTracker()
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -39,29 +56,13 @@ $(".time-block").each(function() {
   // current hour in 24-hour time?
   //
 
-  if (blockTime < timeNow) {
-    $(this).removeClass("future");
-    $(this).removeClass("present");
-    $(this).removeClass("past");
-  }
-
-  else if (block === timeNow) {
-    $(this).removeClass("past");
-    $(this).removeClass("present");
-    $(this).removeClass("future");
-  }
-
-  else {
-    $(this).removeClass("past");
-    $(this).removeClass("present");
-    $(this).removeClass("future");
-  }
+ 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
 
   // Save text and time in local storage.
-  localStorage.setItem(time, text);
+ var savedData
 
 });
